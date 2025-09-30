52d6a15a90ba6f570f9f78d8a7a64d884a3086c4
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-29 22:05:01 -0700

    Added a description

--- src/app/object/page.tsx
+++ src/app/object/page.tsx
@@ -7,6 +7,7 @@ export default function ObjectPage() {
       <h2 className="text-2xl ml-5 mb-2">In Game Name: RSC 8513-2673-2-10-10</h2>
       <h2 className="text-2xl ml-5">Object Type: Star</h2>
       <div className="w-screen h-[1px] bg-white m-4.5 ml-0"></div>
+      <p className="m-5 text-[30px]">Kryo A is a solar system I gave lore when I was bored in space engine. I gave it lore because it seemed cool and I was kinda bored while I was playing space engine. Kryo A has a proper description in game but I dont feel like opening the game to check.</p>
     </>
   );
 }
