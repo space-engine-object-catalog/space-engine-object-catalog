622419f74636dec035f8e96583188bbb419f0195
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-29 22:29:07 -0700

    Moved all of the object data to variables
    This is cuz once this is done it will be getting all this info from the
    server obviously.

--- src/app/object/page.tsx
+++ src/app/object/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 
+import { useState, useEffect } from 'react'
+
 export default function ObjectPage() {
+  const [ObjectName, SetObjectName] = useState<string>('');
+  const [InGameName, SetInGameName] = useState<string>('');
+  const [ObjectType, SetObjectType] = useState<string>('');
+  const [IsRealObject, SetIsRealObject] = useState<string>('');
+  const [Description, SetDescription] = useState<string>('');
+
+  useEffect(() => {
+    SetObjectName("Kryo A");
+    SetInGameName("RSC 8513-2673-2-10-10");
+    SetObjectType("Star");
+    SetIsRealObject("Procedural Object");
+    SetDescription(
+      "Kryo A is a solar system I gave lore when I was bored in space engine. I gave it lore because it seemed cool and I was kinda bored while I was playing space engine. Kryo A has a proper description in game but I dont feel like opening the game to check."
+    );
+  }, []);
+
   return (
     <>
-      <h1 className="text-5xl m-5 mb-2">Kryo A</h1>
-      <h2 className="text-2xl ml-5 mb-2">In Game Name: RSC 8513-2673-2-10-10</h2>
-      <h2 className="text-2xl ml-5 mb-2">Object Type: Star</h2>
-      <h2 className="text-2xl ml-5 mb-4.5">Procedural Object</h2>
+      <h1 className="text-5xl m-5 mb-2">{ObjectName}</h1>
+      <h2 className="text-2xl ml-5 mb-2">In Game Name: {InGameName}</h2>
+      <h2 className="text-2xl ml-5 mb-2">Object Type: {ObjectType}</h2>
+      <h2 className="text-2xl ml-5 mb-4.5">{IsRealObject}</h2>
       <div className="w-screen h-[1px] bg-white mb-4.5"></div>
-      <p className="m-5 mt-0 text-[30px]">Kryo A is a solar system I gave lore when I was bored in space engine. I gave it lore because it seemed cool and I was kinda bored while I was playing space engine. Kryo A has a proper description in game but I dont feel like opening the game to check.</p>
+      <p className="m-5 mt-0 text-[30px]">{Description}</p>
     </>
   );
 }
