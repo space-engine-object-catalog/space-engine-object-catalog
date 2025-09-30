f141a99ada4d5fcc19f1d535de4ef7a45f707755
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-29 21:58:00 -0700

    Started work on the object page
    I'm using a placeholder object rn cuz I wanna make the design of the
    page before I make the logic.

--- src/app/object/page.tsx
+++ src/app/object/page.tsx
@@ -3,7 +3,9 @@
 export default function ObjectPage() {
   return (
     <>
-      <h1>ill add the page later lol</h1>
+      <h1 className="text-5xl m-5 mb-3">Kryo A</h1>
+      <h2 className="text-2xl ml-5">In Game Name: RSC 8513-2673-2-10-10</h2>
+      <div className="w-screen h-[1px] bg-white m-4.5 ml-0"></div>
     </>
   );
 }
