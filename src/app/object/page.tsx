66d487063cf45079edf06801ed4ba65959af2f49
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-30 14:55:10 -0700

    Added more info
    not all of it is displayed yet but I made the objects table in the db so
    I needed to add all the fields from that into the client.

--- src/app/object/page.tsx
+++ src/app/object/page.tsx
@@ -7,16 +7,30 @@ export default function ObjectPage() {
   const [InGameName, SetInGameName] = useState<string>('');
   const [ObjectType, SetObjectType] = useState<string>('');
   const [IsRealObject, SetIsRealObject] = useState<string>('');
+  const [ESI, SetESI] = useState<string>('');
+  const [ChildCount, SetChildCount] = useState<string>('');
+  const [LastUpdated, SetLastUpdated] = useState<string>('');
+  const [DiscoveryDate, SetDiscoveryDate] = useState<string>('');
+  const [SubmittedBy, SetSubmittedBy] = useState<string>('');
+  const [VerificationStatus, SetVerificationStatus] = useState<string>('');
   const [Description, SetDescription] = useState<string>('');
+  const [Tags, SetTags] = useState<string>('');
 
   useEffect(() => {
     SetObjectName("Kryo A");
     SetInGameName("RSC 8513-2673-2-10-10");
     SetObjectType("Star");
     SetIsRealObject("Procedural Object");
+    SetESI("0");
+    SetChildCount("6");
+    SetLastUpdated("9/30/2025, 2:16:23 PM");
+    SetDiscoveryDate("9/23/2025");
+    SetSubmittedBy("Puppet");
+    SetVerificationStatus("verified");
     SetDescription(
       "Kryo A is a solar system I gave lore when I was bored in space engine. I gave it lore because it seemed cool and I was kinda bored while I was playing space engine. Kryo A has a proper description in game but I dont feel like opening the game to check."
     );
+    SetTags("star, solar-system, star-system");
   }, []);
 
   return (
@@ -24,7 +38,10 @@ export default function ObjectPage() {
       <h1 className="text-5xl m-5 mb-2">{ObjectName}</h1>
       <h2 className="text-2xl ml-5 mb-2">In Game Name: {InGameName}</h2>
       <h2 className="text-2xl ml-5 mb-2">Object Type: {ObjectType}</h2>
-      <h2 className="text-2xl ml-5 mb-4.5">{IsRealObject}</h2>
+      <h2 className="text-2xl ml-5 mb-2">{IsRealObject}</h2>
+      <h2 className="text-2xl ml-5 mb-2">Discovered: {DiscoveryDate}</h2>
+      {VerificationStatus == "verified" && <div className='h-2.5'></div>}
+      {VerificationStatus == "unverified" && <h2 className="!text-[#ff9100] text-2xl ml-5 mb-4.5">This article is not verified! It may be inaccurate!</h2>}
       <div className="w-screen h-[1px] bg-white mb-4.5"></div>
       <p className="m-5 mt-0 text-[30px]">{Description}</p>
     </>
