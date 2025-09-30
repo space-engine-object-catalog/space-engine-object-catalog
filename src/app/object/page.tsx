1a44a1a06f0a3796276c6d476329da11bc4bf64e
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-29 22:14:03 -0700

    changed margin stuff

--- src/app/object/page.tsx
+++ src/app/object/page.tsx
@@ -5,9 +5,10 @@ export default function ObjectPage() {
     <>
       <h1 className="text-5xl m-5 mb-2">Kryo A</h1>
       <h2 className="text-2xl ml-5 mb-2">In Game Name: RSC 8513-2673-2-10-10</h2>
-      <h2 className="text-2xl ml-5">Object Type: Star</h2>
-      <div className="w-screen h-[1px] bg-white m-4.5 ml-0"></div>
-      <p className="m-5 text-[30px]">Kryo A is a solar system I gave lore when I was bored in space engine. I gave it lore because it seemed cool and I was kinda bored while I was playing space engine. Kryo A has a proper description in game but I dont feel like opening the game to check.</p>
+      <h2 className="text-2xl ml-5 mb-2">Object Type: Star</h2>
+      <h2 className="text-2xl ml-5 mb-4.5">Procedural Object</h2>
+      <div className="w-screen h-[1px] bg-white mb-4.5"></div>
+      <p className="m-5 mt-0 text-[30px]">Kryo A is a solar system I gave lore when I was bored in space engine. I gave it lore because it seemed cool and I was kinda bored while I was playing space engine. Kryo A has a proper description in game but I dont feel like opening the game to check.</p>
     </>
   );
 }
