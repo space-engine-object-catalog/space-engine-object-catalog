b1a36995b93e328fed48181a5fb7582ebbb75c6b
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-09-29 21:59:12 -0700

    Added text that says the type of object

--- src/app/object/page.tsx
+++ src/app/object/page.tsx
@@ -3,8 +3,9 @@
 export default function ObjectPage() {
   return (
     <>
-      <h1 className="text-5xl m-5 mb-3">Kryo A</h1>
-      <h2 className="text-2xl ml-5">In Game Name: RSC 8513-2673-2-10-10</h2>
+      <h1 className="text-5xl m-5 mb-2">Kryo A</h1>
+      <h2 className="text-2xl ml-5 mb-2">In Game Name: RSC 8513-2673-2-10-10</h2>
+      <h2 className="text-2xl ml-5">Object Type: Star</h2>
       <div className="w-screen h-[1px] bg-white m-4.5 ml-0"></div>
     </>
   );
